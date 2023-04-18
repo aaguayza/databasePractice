4e71583553a0cf5f31ac665d6f994a77eea66ed1
Tony <adaguayza@gmail.com>
2023-04-18 11:23:51 -0500

    first commit

---
+++ Tester2/Data/UserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tester2.Models;
+
+namespace Tester2.Data
+{
+    public class UserDbContext: DbContext
+    {
+        DbSet<Profile> Profiles { get; set; }
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Tester2/Models/Profile.cs
@@ -0,0 +1,17 @@
+﻿namespace Tester2.Models
+{
+    public class Profile
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? UserEmail { get; set; }
+        public Profile()
+        {
+
+        }
+        public Profile(string name)
+        {
+            Name = name;
+        }
+    }
+}
